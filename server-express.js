36d5113b79fbd708810e9a7d385b0c86956bd174
pobi <pobittrachandraroy2001@gmail.com>
2024-04-18 18:03:08 +0600

    intigrate with mongodb and conect with express

--- server-express.js
+++ server-express.js
@@ -1,21 +1,50 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.get("/", (req, res) => {
-  res.json({ name: "pobi", surname: "pobittra" });
-});
-app.get("/home", (req, res) => {
-  res.send("this is home");
-});
-app.get("/card", (req, res) => {
-    res.sendFile(__dirname+ "/index.html");
-  });
+const uri =
+  "mongodb+srv://user1:uservai123@cluster0.i4a55kg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.get("/posts", (req, res) => {
-  res.send("this is posts");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(5000, () => {
-  console.log(`server listening on port ${port}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    app.get("/", (req, res) => {
+      res.json({ name: "pobi", surname: "pobittra" });
+    });
+    app.get("/home", (req, res) => {
+      res.send("this is home");
+    });
+    app.get("/card", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
+
+    app.get("/posts", (req, res) => {
+      res.send("this is posts");
+    });
+
+    app.listen(5000, () => {
+      console.log(`server listening on port ${port}`);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
